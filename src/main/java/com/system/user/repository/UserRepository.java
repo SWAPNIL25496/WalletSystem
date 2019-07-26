f17392e90277aba2e8da34933f4699b8b0e7da06
Swapnil Singh Chauhan <swapnilsinghchauhan@197nodnb24174.one97.delhi.net>
2019-07-26 10:52:37 +0530

    propertied changes

---
+++ src/main/java/com/system/user/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.restapplication.repositories;
+
+import com.restapplication.model.UserData;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+@Repository
+public interface UserRepository extends JpaRepository<UserData, Integer> {
+    boolean existsByEmail(String email);
+    boolean existsByPhoneNumber(String phoneNumber);
+    UserData findByPhoneNumber(String phoneNumber);
+}
