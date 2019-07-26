a03443b9e669c8e789a311db09c67c0206195bac
Swapnil Singh Chauhan <swapnilsinghchauhan@197nodnb24174.one97.delhi.net>
2019-07-26 17:04:38 +0530

    mapper shifted

---
+++ src/main/java/com/system/user/util/UserMapper.java
@@ -0,0 +1,31 @@
+package com.system.user.util;
+
+import com.system.user.dto.UserDto;
+import com.system.user.model.UserData;
+
+import javax.validation.Valid;
+
+public class UserMapper {
+
+    private UserData toEntity(@Valid UserDto userDto) {
+        UserData entity = new UserData();
+        if(userDto.getId()!=null){
+            entity.setId(userDto.getId());
+        }
+        entity.setName(userDto.getName());
+        entity.setEmail(userDto.getEmail());
+        entity.setPhoneNumber(userDto.getPhoneNumber());
+
+        return entity;
+    }
+
+    private UserDto toDto(@Valid  UserData userData) {
+        UserDto entity = new UserDto();
+        entity.setId(userData.getId());
+        entity.setName(userData.getName());
+        entity.setEmail(userData.getEmail());
+        entity.setPhoneNumber(userData.getPhoneNumber());
+
+        return entity;
+    }
+}
